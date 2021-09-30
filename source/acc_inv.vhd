7c89ec9acf0cdce47bb9e430d7accfcdee086344
AivarKu <aivar.klle@protonmail.com>
2021-09-30 11:54:22 +0200

    Added code for Task0

--- source/acc_inv.vhd
+++ source/acc_inv.vhd
@@ -38,25 +38,108 @@ entity acc_inv is
     );
 end acc_inv;
 
---------------------------------------------------------------------------------
--- The desription of the accelerator.
---------------------------------------------------------------------------------
+    
 
 architecture rtl of acc_inv is
 
--- All internal signals are defined here
+    type state_type is (WAIT_START, READ, WRITE, STOP); -- Input your own state names
+    signal state, next_state : state_type;
+
+    signal addr_cnt_ena : std_logic;
+    signal addr_cnt : unsigned(15 downto 0);
+    signal addr_cnt_done : std_logic;
+    
+    constant ram_wr_offset : integer := 25344;
+    
+    signal pixel0 : unsigned(7 downto 0);
+    signal pixel1 : unsigned(7 downto 0);
+    signal pixel2 : unsigned(7 downto 0);
+    signal pixel3 : unsigned(7 downto 0);
 
 begin
 
--- Template for a process
---    myprocess : process(clk)
---        if rising_edge(clk) then
---            if reset = '1' then
---                -- Registers reset
---            else
---                -- Registers update
---            end if;
---        end if;
---    end process myprocess;
+
+    myprocess : process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                addr_cnt <= (others => '0');
+                addr_cnt_done <= '0';
+            else
+                if addr_cnt_ena = '1'  then
+                    if addr_cnt = 25343 then
+                        addr_cnt_done <= '1';
+                        addr_cnt <= (others => '0');
+                    else
+                        addr_cnt <= addr_cnt + 1;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process myprocess;
+    
+    addr <= std_logic_vector(addr_cnt) when we = '0' else
+            std_logic_vector(addr_cnt + ram_wr_offset);
+            
+    pixel0 <= 255-unsigned(dataR(7 downto 0));
+    pixel1 <= 255-unsigned(dataR(15 downto 8));
+    pixel2 <= 255-unsigned(dataR(23 downto 16));
+    pixel3 <= 255-unsigned(dataR(31 downto 24));
+    
+    
+    -- Combinatoriel logic
+    cl : process (all)
+    begin
+        next_state  <= WAIT_START;
+        en <= '0';
+        we <= '0';
+        finish <= '0';
+        addr_cnt_ena <= '0';
+        dataW <= (others => '0');
+    
+        case (state) is       
+            when WAIT_START =>
+                if start = '1' then
+                    next_state  <= READ;
+                end if;   
+             
+            when READ =>
+                en <= '1';
+                next_state  <= WRITE;
+      
+            when WRITE =>
+                en <= '1';
+                we <= '1';
+                addr_cnt_ena <= '1';
+                dataW <= std_logic_vector( pixel3 & pixel2 & pixel1 & pixel0);
+                if addr_cnt_done = '1' then
+                    next_state  <= STOP;
+                else
+                    next_state  <= READ;
+                end if;        
+                
+             when STOP =>
+                en <= '0';
+                finish <= '1';
+                next_state  <= WAIT_START;
+            
+            when others =>
+                next_state <= WAIT_START;
+        
+        end case;
+    end process cl;
+    
+    -- Registers
+    seq : process (clk, reset)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
+                state <= WAIT_START;
+            else
+                state <= next_state;
+            end if;
+        end if;
+    end process seq;
+
 
 end rtl;
--- source/testbench.vhd
+++ source/testbench.vhd
@@ -119,7 +119,7 @@ begin
 
     Memory : memory2
         generic map(
-            load_file_name => "pic1.pgm"
+            load_file_name => "../pic1.pgm"
         )
         -- Result is saved to: load_file_name & "_result.pgm"
         port map(
